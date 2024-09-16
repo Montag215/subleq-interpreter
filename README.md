b1591c3ff64e8d37308dead3b4b12a93e41595a0
Matthew Risley <mqrmqrmqrmqr@gmail.com>
2024-09-15 23:02:18 -0700

    Added stdin support, improved parser and documentation.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # subleq-interpreter
 # subleq interpreter, assembles file inputted as first argument into subleq and runs
+
+address 255: writing to this returns using the written byte as the return value
+254: writing to this write to standard output
+253: reading from this reads from standard input
+252: constant zero byte
--- interpreter.py
+++ interpreter.py
@@ -3,7 +3,8 @@ import sys
 
 def interpret(args):
     if not args:
-        return 1
+        print("usage: interpreter.py FILENAME")
+        return 1 # no input file
     f = open(args[0], 'r')
     instructions = f.read()
     #print(instructions)
@@ -14,24 +15,39 @@ def interpret(args):
     i = 0
     for x in lines:
         for y in x.split():
-            data[i] = int(y)
+            data[i] = abs(int(y)+512)%256
             i += 1
-    print(data)
+            if i>252:
+                break
+        if i>252:
+            break
+    print(data) # prints the program
 
     pc = 0
-    while not data[255]:
-       #print("step")
-        val = data[data[(pc+1)%256]] - data[data[pc]]
-        data[data[(pc+1)%256]] = (val+256)%256
-        if val<=0: 
+    while True: # main program
+        #print("step")
+        data[252] = 0 # constant zero byte
+        x = data[data[(pc+1)%256]] # collect referenced data
+
+        if data[(pc+1)%256] == 253: # std input
+            x = ord(sys.stdin.read(1))
+        y = data[data[pc]]
+        if data[pc] == 253:
+            y = ord(sys.stdin.read(1))
+
+        val = x - y
+        if data[(pc+1)%256] == 255: # return if written to
+            return (val+256)%256
+        
+        data[data[(pc+1)%256]] = (val+256)%256 # assignment
+        if val<=0: # branch?
             pc = data[(pc+2)%256]
         else:
             pc = (pc+3)%256
-        if data[254]:
+
+        if data[254]: # std output
             print(chr(int(data[254])),end='')
             data[254] = 0
-
-    return data[255]
 
 if __name__ == "__main__":
     sys.exit(interpret(sys.argv[1:]))
